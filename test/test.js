b17b668da22254a2fc9934decd3a32b2852d3cf9
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-04 10:41:16 +0200

    ES2015ify tests

--- test/test.js
+++ test/test.js
@@ -1,17 +1,16 @@
-'use strict';
-var path = require('path');
-var test = require('ava');
-var readChunk = require('read-chunk');
-var isWoff2 = require('../');
+import path from 'path';
+import test from 'ava';
+import readChunk from 'read-chunk';
+import isWoff2 from '../';
 
-test('should detect woff2 files', function (t) {
-	var woff2 = readChunk.sync(path.join(__dirname, 'fixture/fixture.woff2'), 0, 8);
+test('should detect woff2 files', t => {
+	const woff2 = readChunk.sync(path.join(__dirname, 'fixture/fixture.woff2'), 0, 8);
 	t.true(isWoff2(woff2));
 
-	var woff = readChunk.sync(path.join(__dirname, 'fixture/fixture.woff'), 0, 8);
+	const woff = readChunk.sync(path.join(__dirname, 'fixture/fixture.woff'), 0, 8);
 	t.false(isWoff2(woff));
 
-	var png = readChunk.sync(path.join(__dirname, 'fixture/fixture.png'), 0, 8);
+	const png = readChunk.sync(path.join(__dirname, 'fixture/fixture.png'), 0, 8);
 	t.false(isWoff2(png));
 
 	t.end();
